87cf71728b8834aa3ceed614c7600b78e56a731c
NH <nh@nhs-macbook-pro.home>
2024-01-18 16:11:31 +0000

    Completed Steps 1 & 2

---
+++ STEP_1&2_poke.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get('https://pokeapi.co/api/v2/pokemon/pikachu')
+
+result = (response.json())
+
+#Gets relevant attributes and stores them into a dict
+new_dict = {"id":result["id"],"name":result["name"],"height":result["height"],"weight":result["weight"]}
+
+print(new_dict)
+
